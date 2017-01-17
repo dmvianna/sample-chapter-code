26a244d64eb295aa244a286399fc689c4ef6c4af
Daniel Vianna <dvianna@bunnings.com.au>
2017-01-18 03:16:17 +0800

    downvote works fine

--- voting_app/app.js
+++ voting_app/app.js
@@ -23,6 +23,15 @@ const ProductList = React.createClass({
     })
     this.updateState()
   },
+  handleProductDownVote: function (productId) {
+    Data.forEach(el => {
+      if (el.id === productId && el.votes > 0) {
+        el.votes = el.votes - 1
+        return
+      }
+    })
+    this.updateState()
+  },
   render: function () {
     const products = this.state.products.map(product => {
     return (
@@ -35,7 +44,8 @@ const ProductList = React.createClass({
         votes={product.votes}
         submitter_avatar_url={product.submitter_avatar_url}
         product_image_url={product.product_image_url}
-        onVote={this.handleProductUpVote}
+        onUpVote={this.handleProductUpVote}
+        onDownVote={this.handleProductDownVote}
       />
     )
     })
@@ -49,7 +59,10 @@ const ProductList = React.createClass({
 
 const Product = React.createClass({
   handleUpVote: function () {
-    this.props.onVote(this.props.id)
+    this.props.onUpVote(this.props.id)
+  },
+  handleDownVote: function () {
+    this.props.onDownVote(this.props.id)
   },
   render: function () {
     return (
@@ -61,6 +74,8 @@ const Product = React.createClass({
           <div className='header'>
             <a onClick={this.handleUpVote}>
               <i className='large caret up icon'></i>
+            </a>
+            <a onClick={this.handleDownVote}>
               <i className='large caret down icon'></i>
             </a>
             {this.props.votes}
